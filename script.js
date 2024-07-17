8aa95b54006c6f9571db4b54de392e4bb0f54602
Alkeo123123 <alkeocepani7@gmail.com>
2024-07-17 15:08:00 +0200

    second commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let personi = {
   
   function Detyra(person, withdraw) {
     let Balance = parseInt(person.CurrentBalance);
-    if (Balance >= withdraw) {
+    if (Balance >= withdraw){
       console.log(`Ti ke para te mjaftueshme per te terhequr ${withdraw}. Balanca e re: ${Balance - withdraw}`);
     } else {
       console.log(`Nuk ke para te mjaftueshme per kete transaksion, te duhen ${withdraw - Balance} para per te perfunduar transaksionin.`);
